2877749e1f981c1874cb86bf4e9c8096731b7c60
Rafael Malon <divino07rafa@gmail.com>
2019-03-24 19:25:11 -0300

    Minor changes

---
+++ reddit_bot.py
@@ -0,0 +1,9 @@
+from praw import *
+#from menu_generator import *
+
+reddit = Reddit('bot1')
+sub = reddit.subreddit('krunkerio')
+
+for post in sub.top(limit=20):
+	if any(n in ['nice', 'man', 'Update'] for n in post.title.split(' ')):
+			post.reply('hello fellow krunkers!!!')
